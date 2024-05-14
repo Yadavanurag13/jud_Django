69e64582776c8d08a7b542f5020cc7fc79730878
Anurag Yadav <yadavanurag1310@gmail.com>
2024-05-14 23:51:09 +0530

    setup all templates

--- a_posts/models.py
+++ a_posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=500) #charField has limit wherease textField() don't
+    image = models.URLField(max_length=500)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.CharField(max_length=100, default=uuid.uuid4, unique=True, primary_key=True, editable=False)
--- a_posts/views.py
+++ a_posts/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
 
-def home_view(request):
-    title = 'Welcome to Django'
-    return render(request, 'index.html', {'title': title})
+def home_view(request):    
+    return render(request, 'a_posts/home.html',)
