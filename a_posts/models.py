3907bcc1dc2fd4a99e5b3fbe78e337ad789a493d
Anurag Yadav <yadavanurag1310@gmail.com>
2024-05-15 17:44:53 +0530

    setup create post functionality

--- a_posts/models.py
+++ a_posts/models.py
@@ -6,4 +6,10 @@ class Post(models.Model):
     image = models.URLField(max_length=500)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
-    id = models.CharField(max_length=100, default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    id = models.CharField(max_length=100, default=uuid.uuid4, unique=True, primary_key=True, editable=False) # type: ignore
+
+    def __str__(self):
+        return str(self.title)
+    
+    class Meta:
+        ordering = ['-created']
--- a_posts/views.py
+++ a_posts/views.py
@@ -1,4 +1,33 @@
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import redirect, render
+from .models import *
+from django.forms import ModelForm
 
-def home_view(request):    
-    return render(request, 'a_posts/home.html',)
+def home_view(request):
+    posts = Post.objects.all()
+    return render(request, 'a_posts/home.html', {'posts': posts})
+
+##function should be wriiten with smallletter starting and using underscore
+
+class PostCreateForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = '__all__'
+        labels = {
+            'body': 'caption',
+        }
+        widgets = {
+    'body': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Add a caption ....', 'class': 'font1 text-4xl'})
+}
+
+def post_create_view(request):
+    form = PostCreateForm() ## class should be written in camelcase
+
+    if request.method == 'POST':
+        form = PostCreateForm(request.POST)
+
+        if form.is_valid:
+            form.save()
+            return redirect('home')
+    return render(request, 'a_posts/post_create.html', {'form': form})
+
